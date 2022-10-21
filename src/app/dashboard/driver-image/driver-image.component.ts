5d09cdbb0fe27b76715c8edd204ee4830cc2f12b
Trungu Xhani <Trungu.Xhani@arhs-dev-hellas.com>
2022-10-21 18:17:48 +0300

    add comment on how to get the main image from wikipedia article of the driver

--- src/app/dashboard/driver-image/driver-image.component.ts
+++ src/app/dashboard/driver-image/driver-image.component.ts
@@ -12,4 +12,6 @@ export class DriverImageComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=Charles_Leclerc
+  // To get the image of a driver you need to get the page id of the driver and then use that id to get the image url from the pageimages property
 }
